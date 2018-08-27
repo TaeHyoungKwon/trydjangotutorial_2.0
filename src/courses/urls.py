1404cc47bf4259a89cc52166c7443e903728bb07
thkwon <TaeHYoungKwon@github.com>
2018-08-27 13:28:10 +0900

    42 - Raw List Class Based View

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
-    CourseView
+    CourseView,
+    CourseListView
 )
 
 app_name = 'courses'
 urlpatterns = [
     #path('', CourseView.as_view(template_name='pages/contact.html'),name='courses-list')
-    path('<int:id>/', CourseView.as_view(), name='courses-detail')
+    #path('<int:id>/', CourseView.as_view(), name='courses-detail'),
+    path('', CourseListView.as_view(), name='courses-list')
 ]
